d925b84b62dd317456df266d871cd10db20a12f1
hanif_albaaits <hanifalbaaits@gmail.com>
2021-01-25 03:08:36 +0700

    update

---
+++ application/views/page/promote.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <base href="<?php echo base_url(); ?>">
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Aplikasi Jual Pulsa dan Paket Data termurah">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets_custom/pavicon.png">
+    <!-- Page Title  -->
+    <title>Promosi | GetId - Aplikasi Jual Pulsa dan Paket Data termurah</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/dashlite.css?ver=1.9.2">
+    <link id="skin" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/theme.css?ver=1.9.2">
+</head>
+
+<body class="nk-body npc-invest bg-lighter ">
+    <div class="nk-app-root">
+        <!-- wrap @s -->
+        <div class="nk-wrap ">
+            <!-- main header @s -->
+            <div class="nk-header nk-header-fluid is-theme" style="background-color : #089aeb">
+                <div class="container-xl wide-xl">
+                    <div class="nk-header-wrap">
+                        <div class="nk-menu-trigger mr-sm-2 d-lg-none">
+                            <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="headerNav"><em
+                                    class="icon ni ni-menu"></em></a>
+                        </div>
+                        <div class="nk-header-brand">
+                            <a href="<?php echo site_url("home"); ?>" class="logo-link">
+                                <img class="logo-light logo-img"
+                                    src="<?php echo base_url(); ?>assets_custom/nama_putih.png"
+                                    srcset="<?php echo base_url(); ?>assets_custom/nama_putih.png 2x" alt="logo">
+                                <img class="logo-dark logo-img"
+                                    src="<?php echo base_url(); ?>assets_custom/nama_putih.png"
+                                    srcset="<?php echo base_url(); ?>assets_custom/nama_putih.png 2x" alt="logo-dark">
+                            </a>
+                        </div><!-- .nk-header-brand -->
+                        <div class="nk-header-menu" data-content="headerNav">
+                            <div class="nk-header-mobile">
+                                <div class="nk-header-brand">
+                                    <a href="<?php echo site_url("home"); ?>" class="logo-link">
+                                        <img class="logo-light logo-img"
+                                            src="<?php echo base_url(); ?>assets_custom/nama_putih.png"
+                                            srcset="<?php echo base_url(); ?>assets_custom/nama_putih.png 2x"
+                                            alt="logo">
+                                        <img class="logo-dark logo-img"
+                                            src="<?php echo base_url(); ?>assets_custom/nama_putih.png"
+                                            srcset="<?php echo base_url(); ?>assets_custom/nama_putih.png 2x"
+                                            alt="logo-dark">
+                                    </a>
+                                </div>
+                                <div class="nk-menu-trigger mr-n2">
+                                    <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="headerNav"><em
+                                            class="icon ni ni-arrow-left"></em></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- .nk-header-wrap -->
+                </div><!-- .container-fliud -->
+            </div>
+            <!-- main header @e -->
+            <!-- content @s -->
+            <div class="nk-content nk-content-fluid">
+                <div class="container-xl wide-xl">
+                    <div class="nk-content-inner">
+                        <div class="nk-content-body">
+                            <div class="content-page">
+                                <div class="nk-block-head nk-block-head-lg wide-sm">
+                                    <div class="nk-block-head-content">
+                                        <h2 class="nk-block-title fw-normal">Promosi</h2>
+                                        <div class="nk-block-des">
+                                            <p class="lead"></p>
+                                            <p class="text-soft ff-italic">Perubahan Terakhir: 25 Januari 2021</p>
+                                        </div>
+                                    </div>
+                                </div><!-- .nk-block-head -->
+                                <div class="nk-block">
+                                    <div class="card card-bordered">
+                                        <div class="card-inner card-inner-xl">
+
+                                        </div><!-- .card-inner -->
+                                    </div><!-- .card -->
+                                </div><!-- .nk-block -->
+                            </div><!-- .content-page -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content @e -->
+            <!-- footer @s -->
+            <div class="nk-footer nk-footer-fluid bg-lighter">
+                <div class="container-xl">
+                    <div class="nk-footer-wrap">
+                        <div class="nk-footer-copyright"> &copy; 2020 GetId </div>
+                        <div class="nk-footer-links">
+                            <ul class="nav nav-sm">
+                                <li class="nav-item"><a class="nav-link"
+                                        href="<?php echo site_url('Help/terms_condition');?>">Persyaratan dan
+                                        Ketentuan</a>
+                                </li>
+                                <li class="nav-item"><a class="nav-link"
+                                        href="<?php echo site_url('Help/privacy_policy');?>">Kebijakan Privasi</a>
+                                </li>
+                                <li class="nav-item"><a class="nav-link"
+                                        href="<?php echo site_url('Help');?>">Bantuan</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- footer @e -->
+        </div>
+        <!-- wrap @e -->
+    </div>
+    <!-- app-root @e -->
+    <!-- JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/js/bundle.js?ver=1.9.2"></script>
+    <script src="<?php echo base_url(); ?>assets/js/scripts.js?ver=1.9.2"></script>
+</body>
+
+</html>
